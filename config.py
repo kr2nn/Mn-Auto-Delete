3eff06df4f0e16cfd8f54fb0a08786b42aacc601
MN-TG <mohammedmusammiln@gmail.com>
2025-05-25 18:09:51 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,21 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
+
+class BOT:
+    TOKEN = os.environ.get("TOKEN", "")
+
+class API:
+    HASH = os.environ.get("API_HASH", "")
+    ID = int(os.environ.get("API_ID", 0))
+
+class OWNER:
+    ID = int(os.environ.get("OWNER", 0))
+
+class WEB:
+    PORT = int(os.environ.get("PORT", 8000))
+
+class CHATS:
+    IDS = list(map(int, os.environ.get("CHATS", "").split())) if os.environ.get("CHATS") else []
+    DELETE_DELAY = int(os.environ.get("DELETE_DELAY", 5))

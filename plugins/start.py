6c1aeca53abfea10bacdca399fbd06ee21511af6
kr2nn <teamcmuog@gmail.com>
2025-07-10 23:22:33 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -2,4 +2,4 @@ from pyrogram import Client, filters
 
 @Client.on_message(filters.private & filters.command("start"))
 async def start(client, message):
-    await message.reply_text("Hey! This bot only works in its specific groups. Repo https://github.com/MN-BOTS/Mn-Auto-Delete ") # @MrMNTG Or @MusammilN 
+    await message.reply_text("Hey! This bot only works in its specific groups.") # @MrMNTG Or @MusammilN 
